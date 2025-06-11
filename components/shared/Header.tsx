1e14afc79beb2d43e657e9ae8c4110146f8c83e3
Djkde01 <sergio.estrella@utp.edu.co>
2025-06-10 22:12:48 -0500

    fix: add missing border color to header component

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -7,7 +7,7 @@ import MobileNav from "./MobileNav"
 
 const Header = () => {
     return (
-        <header className="w-full border-b">
+        <header className="w-full border-b border-border">
             <div className="wrapper flex items-center justify-between">
                 <Link href="/" className="w-36">
                     <Image src="/assets/images/logo-backbone.svg" alt="Logo Backbone" width={130} height={30} />
