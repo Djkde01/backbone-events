1842f671fc31a5d2b8aad4ec8eca9e5c49087a4f
Djkde01 <sergio.estrella@utp.edu.co>
2025-06-10 22:12:31 -0500

    feat: add Footer component with logo and copyleft information

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const Footer = () => {
+    return (
+        <footer className="border-t border-border">
+            <div className="flex items-center justify-between wrapper py-4 flex-col md:flex-row gap-4">
+                <Link href="/" className="w-36">
+                    <Image src="/assets/images/logo-backbone.svg" alt="Logo Backbone" width={130} height={30} />
+                </Link>
+                <p className="text-[16px] leading-[24px] text-primary-50">
+                    🄯 {new Date().getFullYear()} Backbone. Designed and developed by{" "}
+                    <Link href="https://sergioestrella.com" target="_blank" rel="noopener noreferrer">
+                        Djkde
+                    </Link>
+                </p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
