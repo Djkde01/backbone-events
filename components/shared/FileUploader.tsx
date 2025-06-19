5792ee20ac653455a33b5a809b828b80075ea2e0
Djkde01 <sergio.estrella@utp.edu.co>
2025-06-19 16:31:19 -0500

    fix: update color naming from grey to gray and improve upload prompt text

--- components/shared/FileUploader.tsx
+++ components/shared/FileUploader.tsx
@@ -22,7 +22,7 @@ const FileUploader = (
         accept: generateClientDropzoneAccept(['image/png', 'image/jpeg', 'image/svg+xml']),
     });
     return (
-        <div {...getRootProps()} className="flex items-center justify-center bg-dark-3 flex h-72 cursor-pointer flex-col overflow-hidden rounded-xl bg-grey-50">
+        <div {...getRootProps()} className="flex items-center justify-center bg-dark-3 flex h-72 cursor-pointer flex-col overflow-hidden rounded-xl bg-gray-50">
             <input {...getInputProps()} />
             {imageUrl ? (
                 <div className="flex h-full w-full flex-1 justify-center ">
@@ -35,12 +35,12 @@ const FileUploader = (
                     />
                 </div>
             ) : (
-                <div className="flex items-center justify-center flex-col py-5 text-grey-500">
+                <div className="flex items-center justify-center flex-col py-5 text-gray-500">
                     <Image src="/assets/icons/upload.svg" width={77} height={77} alt="file upload" />
-                    <h3 className="mb-2 mt-2">Arrastra una foto aquí</h3>
+                    <h3 className="mb-2 mt-2">Arrastra y sube una foto</h3>
                     <p className="p-medium-12 mb-4">SVG, PNG, JPG</p>
                     <Button type="button" className="rounded-full">
-                        Seleccionar desde el dispositivo
+                        Selecciona desde tu dispositivo
                     </Button>
                 </div>
             )}
