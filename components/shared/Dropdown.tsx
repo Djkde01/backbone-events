5e5f15f68af98e4cdcfbd08ee9b4bfa608e2cca0
Djkde01 <sergio.estrella@utp.edu.co>
2025-06-20 00:11:33 -0500

    feat: enhance Dropdown component with category fetching and addition functionality

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -16,10 +16,11 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Input } from "../ui/input"
 import { DialogClose } from "@radix-ui/react-dialog"
 import { Button } from "../ui/button"
+import { createCategory, getAllCategories } from "@/lib/actions/caregory.actions"
 
 const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
     const [categories, setCategories] = useState<ICategory[]>([
@@ -28,11 +29,24 @@ const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
     const [newCategory, setNewCategory] = useState<string>("")
 
     const handleAddCategory = () => {
-        console.log("Añadir categoría:", newCategory)
+        createCategory({ categoryName: newCategory.trim() }).then((category) => {
+            setCategories((prev) => [...prev, category])
+        })
     }
+
+    useEffect(() => {
+        const getCategories = async () => {
+            const categoryList = await getAllCategories()
+            if (categoryList.length === 0) return
+            setCategories(categoryList as ICategory[])
+        }
+
+
+        getCategories()
+    }, [])
     return (
         <Select onValueChange={onChangeHandler} defaultValue={value}>
-            <SelectTrigger className="select-field">
+            <SelectTrigger className="select-field h-[54px]">
                 <SelectValue placeholder="Categoría" />
             </SelectTrigger>
             <SelectContent>
@@ -47,7 +61,7 @@ const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
                     ))
                 }
                 <Dialog>
-                    <DialogTrigger className="w-full flex hover:bg-primary-50">Añadir categoría</DialogTrigger>
+                    <DialogTrigger className="w-full flex hover:bg-primary-50 m-2 cursor-pointer">Añadir categoría</DialogTrigger>
                     <DialogContent>
                         <DialogHeader>
                             <DialogTitle>Nueva Categoría</DialogTitle>
@@ -57,9 +71,9 @@ const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
                         </DialogHeader>
                         <DialogFooter>
                             <DialogClose asChild>
-                                <Button variant="outline">Cancelar</Button>
+                                <Button className="cursor-pointer" variant="outline">Cancelar</Button>
                             </DialogClose>
-                            <Button type="submit" onClick={handleAddCategory}>Añadir</Button>
+                            <Button className="cursor-pointer" type="submit" onClick={handleAddCategory}>Añadir</Button>
                         </DialogFooter>
                     </DialogContent>
                 </Dialog>
